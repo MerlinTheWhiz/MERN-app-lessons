88830ee467c8c973c20b0281c1e5372abda182e9
MerlinTheWhiz <michaelnathan505@gmail.com>
2025-07-30 07:09:33 +0100

    Third commit 1

--- server.js
+++ server.js
@@ -1,26 +1,13 @@
 import express from "express"; //cannot use this by default, you must change type to module for this
 //const express = require("express"); //can use this by default, same function with the above
+import notesRoutes from "./routes/noteRoutes.js";
 
 const app = express(); //
 
-//to build an api route
-//in the bracket, the quotation is what we're listening to(the end point) and then a function to come after the comma
-//the entire line is called a route
-app.get("/api/notes", (req, res) => {
-  res.status(200).send("you got 10 notes");
-});
+app.use("/api/notes", notesRoutes)
 
-app.post("/api/notes", (req, res)=> {
-  res.status(201).json("Note created successfully");
-});
 
-app.put("/api/notes/:id", (req, res) => {
-  res.status(200).json("Note updated successfully");
-});
 
-app.delete("/api/notes/:id", (req, res) => {
-  res.status(200).json("Note updated successfully");
-});
 
 //to listen on a port
 app.listen(5300, () => {
