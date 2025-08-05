f89c425900ef32c9e5305677e6818f980881da7a
MerlinTheWhiz <michaelnathan505@gmail.com>
2025-08-06 02:09:45 +0700

    Fifth commit

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,16 +1,68 @@
-export function getAllNotes (req, res) {
-    res.status(200).send("you got 10 notes");
-};
+import Note from "../models/Note.js";
 
-export function createNote (req, res) {
-  res.status(201).json("Note created successfully");
-};
+export async function getAllNotes(_, res) {
+  try {
+    const notes = await Note.find().sort({ createdAt: -1 }); //to get all notes //from the latest addition
+    res.status(200).json(notes);
+  } catch (error) {
+    console.error("Error in getAllNotes controller", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
 
-export function updateNote (req, res) {
-  res.status(201).json("Note updated successfully");
-};
+export async function getNoteById(req, res) {
+  try {
+    const note = await Note.findById(req.params.id); // find a specific note by id
+    if (!note) return res.status(404).json({ message: "Note not found" });
+    res.status(200).json(note);
+  } catch (error) {
+    console.error("Error in getNoteById controller", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
 
-export function deleteNote (req, res) {
-  res.status(201).json("Note deleted successfully");
-};
+export async function createNote(req, res) {
+  try {
+    // creating new note
+    const { title, content } = req.body; // taking input
+    const newNote = new Note({ title: title, content: content }); // you can shorten this by just putting the variable, since the key and variable names are the same
 
+    // save to db
+    const savedNote = await newNote.save();
+    res.status(201).json(savedNote);
+  } catch (error) {
+    console.error("Error in createNote controller", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function updateNote(req, res) {
+  try {
+    const { title, content } = req.body;
+    const updatedNote = await Note.findByIdAndUpdate(
+      req.params.id,
+      { title, content },
+      { new: true }
+    );
+    if (!updatedNote)
+      return res.status(404).json({ message: "Note not found" });
+
+    res.status(200).json(updatedNote);
+  } catch (error) {
+    console.error("Error in createNote controller", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function deleteNote(req, res) {
+  try {
+    const deletedNote = await Note.findByIdAndDelete(req.params.id);
+    if (!deletedNote)
+      return res.status(404).json({ message: "Note not found" });
+
+    res.status(200).json({ message: "Note deleted successfully" });
+  } catch (error) {
+    console.error("Error in deleteNote controller", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
--- src/server.js
+++ src/server.js
@@ -2,16 +2,22 @@ import express from "express";
 import notesRoutes from "./routes/noteRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
+console.log(process.env.MONGO_URI)
 
 const app = express();
 const PORT = process.env.PORT || 5300;
 
-connectDB()
 
-app.use("/api/notes", notesRoutes)
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
+app.use(rateLimiter); // middleware for ratelimiters
 
-app.listen(PORT, () => {
+app.use("/api/notes", notesRoutes); // routes middleware
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
   console.log("Server started on PORT: ", PORT);
+})
 });
