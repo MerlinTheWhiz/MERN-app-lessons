c80421dc62be6e6e56a03e6451772a6efd6c0f9a
MerlinTheWhiz <michaelnathan505@gmail.com>
2025-08-14 20:29:05 +0100

    Frontend 4

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,9 +2,10 @@ import React, { useEffect } from 'react';
 import { useState } from 'react';
 import Navbar from '../components/Navbar';
 import RateLimitedUI from '../components/RateLimitedUI';
-import axios from 'axios';
+import api from '../lib/axios.js';
 import toast from 'react-hot-toast';
 import NoteCard from "../components/NoteCard";
+import NotesNotFound from '../components/NotesNotFound';
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -15,7 +16,7 @@ const HomePage = () => {
   useEffect(() => {
     const fetchNotes = async () => {
     try {
-      const res = await axios.get("http://localhost:5300/api/notes");
+      const res = await api.get("/notes");
       console.log(res.data);
       setNotes(res.data);
       setIsRateLimited(false);
@@ -43,6 +44,8 @@ const HomePage = () => {
 
       <div className="max-w-7xl mx-auto p-4 mt-6">
         {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
+
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
 
         {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
