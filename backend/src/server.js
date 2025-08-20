6462c524a0a7fb54133296b6d6ecee73a3cb3e70
MerlinTheWhiz <michaelnathan505@gmail.com>
2025-08-20 22:22:27 +0100

    Final Commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,10 @@
-import express from "express"; 
-import notesRoutes from "./routes/noteRoutes.js";
+import express from "express";
 import cors from "cors";
-
-import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import path from "path";
+
+import { connectDB } from "./config/db.js"; 
+import notesRoutes from "./routes/noteRoutes.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
@@ -11,10 +12,14 @@ console.log(process.env.MONGO_URI)
 
 const app = express();
 const PORT = process.env.PORT || 5300;
+const __dirname = path.resolve()
+console.log("path name is",__dirname)
 
-app.use(cors({
-  origin: "http://localhost:5173"
-}));
+if(process.env.NODE_ENV !== "production"){
+  app.use(cors({
+    origin: "http://localhost:5173"
+  }));
+}
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter); // middleware for ratelimiters
 
@@ -26,6 +31,14 @@ app.use(rateLimiter); // middleware for ratelimiters
 // });
 
 app.use("/api/notes", notesRoutes); // routes middleware
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist","index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
